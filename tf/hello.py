896f5d3d079782c784beb36abc9d1bee98a0b57a
gooneys <fei.jiang72@gmail.com>
2019-01-20 21:01:36 +0800

    this is the original file

---
+++ tf/hello.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+print "hello, python world"
+print "use to check if the git works fine"
+print "2nd check the git command"
+print "gvim have some error? and set noundofile, nobackup to .vmrc"
